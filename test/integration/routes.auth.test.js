c94568f13e20450a46665ca072f39b3ae22b18f4
Omegamark <grant.mark@gmail.com>
2017-06-12 12:22:50 -0600

    trying to figure out chai test

--- test/integration/routes.auth.test.js
+++ test/integration/routes.auth.test.js
@@ -21,3 +21,22 @@ describe('routes : auth', () => {
   });
 
 });
+
+describe('POST /auth/register', () => {
+  it('should register a new user', (done) => {
+    chai.request(server)
+    .post('/auth/register')
+    .send({
+      username: 'michael',
+      password: 'herman'
+    })
+    .end((err, res) => {
+      should.not.exist(err);
+      res.redirects.length.should.eql(0);
+      res.status.should.eql(200);
+      res.type.should.eql('application/json');
+      res.body.status.should.eql('success');
+      done();
+    });
+  });
+});
